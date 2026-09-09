7ec67b88febf7338013c09a924a3d7bee67042e5
Jack Guilfoyle <jackguilfoyle@gmail.com>
2026-09-09 15:58:32 +0100

    cleaned up app.jsx and took our some unnecessary react modules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,51 @@
-import { useEffect, useRef, useState } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Home from "./Home.jsx";
 import ScanPage from "./ScanPage.jsx";
 import Register from "./Register.jsx";
-import AddBook from './AddBook.jsx';
-import Book from './Book.jsx';
-import FlashcardsPage from './flashcards.jsx';
+import AddBook from "./AddBook.jsx";
+import Book from "./Book.jsx";
+import FlashcardsPage from "./flashcards.jsx";
 
 function App() {
-
   return (
-   <Routes>
-      <Route path="/" element={<Navigate to="/register" />} />
-      <Route path="/book/:bookId/scan" element={<ScanPage />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/add-book" element={<AddBook/>}/>
-      <Route path="/book/:bookId" element={<Book/>}/>
-      <Route path="/flashcards/:bookId" element={<FlashcardsPage />}/>
-   </Routes>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigate to="/register" />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
+      <Route
+        path="/home"
+        element={<Home />}
+      />
+
+      <Route
+        path="/add-book"
+        element={<AddBook />}
+      />
+
+      <Route
+        path="/book/:bookId"
+        element={<Book />}
+      />
+
+      <Route
+        path="/book/:bookId/scan"
+        element={<ScanPage />}
+      />
+
+      <Route
+        path="/flashcards/:bookId"
+        element={<FlashcardsPage />}
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
